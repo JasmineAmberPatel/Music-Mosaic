d951c2e8e0c7ae5f2e9b48670b7a1e7801307f23
Jasmine Amber <jasmineamber.ja@gmail.com>
2020-04-27 13:01:42 +0100

    can retrieve now playing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import SpotifyWebApi from 'spotify-web-api-js';
+const spotifyApi = new SpotifyWebApi();
+
+
+class App extends Component {
+  constructor() {
+    super();
+    const params = this.getHashParams();
+    const token = params.access_token;
+    if (token) {
+      spotifyApi.setAccessToken(token);
+    }
+    this.state = {
+      loggedIn: token ? true : false,
+      nowPlaying: { name: '', albumArt: '' }
+    }
+  }
+  getHashParams() {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+    e = r.exec(q)
+    while (e) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
+      e = r.exec(q);
+    }
+    return hashParams;
+  }
+
+  getNowPlaying() {
+    spotifyApi.getMyCurrentPlaybackState()
+      .then((response) => {
+        this.setState({
+          nowPlaying: {
+            name: response.item.name,
+            albumArt: response.item.album.images[0].url
+          }
+        });
+      })
+      .catch(error => alert('cannot retrieve spotify information right'));
+  }
+
+  render() {
+    console.log(this.state);
+    return (
+      <div className="App">
+        <div className='login'>
+          <a href='http://localhost:8888'> Login to Spotify </a>
+        </div>
+        <div className="Paragraph">
+          {this.state.nowPlaying.name}
+        </div>
+        <div>
+          <img src={this.state.nowPlaying.albumArt} style={{ height: 150 }} alt="album art" />
+        </div>
+        {this.state.loggedIn &&
+          <button onClick={() => this.getNowPlaying()} className="btn btn-dark">Check Spotify Now Playing</button>
+        }
+      </div>
+    );
+  }
 }
+
 
 export default App;
