3ab1144e81813bff3f51d5d378815d0ec00e36f9
Jasmine Amber <jasmineamber.ja@gmail.com>
2020-05-28 23:25:51 +0100

    added logged in state

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 import './index.css';
 import queryString from 'query-string';
 
+let parsed = queryString.parse(window.location.search);
+let accessToken = parsed.access_token;
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
+      loggedIn: accessToken ? true : false,
       name: '',
       recentlyPlayed: {
         albumArt: ''
@@ -14,8 +18,6 @@ class App extends Component {
   }
 
   componentDidMount() {
-    let parsed = queryString.parse(window.location.search);
-    let accessToken = parsed.access_token;
     if (!accessToken)
       return;
     fetch('https://api.spotify.com/v1/me', {
@@ -28,38 +30,39 @@ class App extends Component {
     }).then(response => response.json())
       .then(data => this.setState({
         recentlyPlayed: data.items.map(item => {
-            return {
-              albumArt: item[0].track.album.images[0].url,
-            }
-          })
-        }))
+          return {
+            albumArt0: item[0].track.album.images[0].url,
+          }
+        })
+      }))
+      .catch(error => alert('cannot retrieve spotify information right'));
   }
 
   getRecentlyPlayed() {
-    return this.state.recentlyPlayed
+    return this.state.recentlyPlayed.albumArt
   }
 
 
 
-render() {
-  console.log(this.state);
-  return (
-    <div className="App">
-      <br />
-      <div className="title">
-        {`${this.state.name}'s Recently Played Mosaic`}
+  render() {
+    console.log(this.state);
+    return (
+      <div className="App">
+        <br />
+        <div className="title">
+          {`${this.state.name}'s Recently Played Mosaic`}
+        </div>
+        <br />
+        <div className="Mosaic">
+          <img src={this.state.recentlyPlayed.albumArt} style={{ height: 50 }} alt="album art" />
+        </div>
+        <div className="buttons">
+          <button onClick={() => this.getRecentlyPlayed()} className="button">Check Spotify Mosaic</button>
+          <button onClick={() => window.location = 'https://spotify-mosaic-backend.herokuapp.com/login'} className="button">Login to Spotify</button>
+        </div>
       </div>
-      <br />
-      <div className="Mosaic">
-        <img src={this.state.recentlyPlayed[0].albumArt} style={{ height: 50 }} alt="album art" />
-      </div>
-      <div className="buttons">
-        <button onClick={() => this.getRecentlyPlayed()} className="button">Check Spotify Mosaic</button>
-        <button onClick={() => window.location = 'https://spotify-mosaic-backend.herokuapp.com/login'} className="button">Login to Spotify</button>
-      </div>
-    </div>
-  );
-}
+    );
+  }
 }
 
 export default App;
