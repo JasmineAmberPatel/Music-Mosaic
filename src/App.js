e74cfaed297569b2f85233d07f5447dec10d2bcc
Jasmine Amber <jasmineamber.ja@gmail.com>
2020-05-28 14:58:51 +0100

    values array method

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ class App extends Component {
   }
 
   getRecentlyPlayed() {
-    return this.state.recentlyPlayed.albumArt
+    return this.state.recentlyPlayed.albumArt.values()
   }
 
 
@@ -51,7 +51,7 @@ render() {
       </div>
       <br />
       <div className="Mosaic">
-        <img src={this.state.recentlyPlayed[0].albumArt} style={{ height: 50 }} alt="album art" />
+        <img src={this.getRecentlyPlayed} style={{ height: 50 }} alt="album art" />
       </div>
       <div className="buttons">
         <button onClick={() => this.getRecentlyPlayed()} className="button">Check Spotify Mosaic</button>
