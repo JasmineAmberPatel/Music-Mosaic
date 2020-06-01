00ef1583807769e7d0b9f6687c3b9326408ec243
Jasmine Amber <jasmineamber.ja@gmail.com>
2020-06-01 13:42:09 +0100

    changed api call

--- src/App.js
+++ src/App.js
@@ -78,61 +78,60 @@ class App extends Component {
       headers: { 'Authorization': 'Bearer ' + accessToken }
     }).then(response => response.json())
       .then(data => this.setState({
-        recentlyPlayed: data.items.map(item => {
-          return {
-            albumArt0: item[0].track.album.images[0].url,
-            albumArt1: item[1].track.album.images[0].url,
-            albumArt2: item[2].track.album.images[0].url,
-            albumArt3: item[3].track.album.images[0].url,
-            albumArt4: item[4].track.album.images[0].url,
-            albumArt5: item[5].track.album.images[0].url,
-            albumArt6: item[6].track.album.images[0].url,
-            albumArt7: item[7].track.album.images[0].url,
-            albumArt8: item[8].track.album.images[0].url,
-            albumArt9: item[9].track.album.images[0].url,
-            albumArt10: item[10].track.album.images[0].url,
-            albumArt11: item[11].track.album.images[0].url,
-            albumArt12: item[12].track.album.images[0].url,
-            albumArt13: item[13].track.album.images[0].url,
-            albumArt14: item[14].track.album.images[0].url,
-            albumArt15: item[15].track.album.images[0].url,
-            albumArt16: item[16].track.album.images[0].url,
-            albumArt17: item[17].track.album.images[0].url,
-            albumArt18: item[18].track.album.images[0].url,
-            albumArt19: item[19].track.album.images[0].url,
-            albumArt20: item[20].track.album.images[0].url,
-            albumArt21: item[21].track.album.images[0].url,
-            albumArt22: item[22].track.album.images[0].url,
-            albumArt23: item[23].track.album.images[0].url,
-            albumArt24: item[24].track.album.images[0].url,
-            albumArt25: item[25].track.album.images[0].url,
-            albumArt26: item[26].track.album.images[0].url,
-            albumArt27: item[27].track.album.images[0].url,
-            albumArt28: item[28].track.album.images[0].url,
-            albumArt29: item[29].track.album.images[0].url,
-            albumArt30: item[30].track.album.images[0].url,
-            albumArt31: item[31].track.album.images[0].url,
-            albumArt32: item[32].track.album.images[0].url,
-            albumArt33: item[33].track.album.images[0].url,
-            albumArt34: item[34].track.album.images[0].url,
-            albumArt35: item[35].track.album.images[0].url,
-            albumArt36: item[36].track.album.images[0].url,
-            albumArt37: item[37].track.album.images[0].url,
-            albumArt38: item[38].track.album.images[0].url,
-            albumArt39: item[39].track.album.images[0].url,
-            albumArt40: item[40].track.album.images[0].url,
-            albumArt41: item[41].track.album.images[0].url,
-            albumArt42: item[42].track.album.images[0].url,
-            albumArt43: item[43].track.album.images[0].url,
-            albumArt44: item[44].track.album.images[0].url,
-            albumArt45: item[45].track.album.images[0].url,
-            albumArt46: item[46].track.album.images[0].url,
-            albumArt47: item[47].track.album.images[0].url,
-            albumArt48: item[48].track.album.images[0].url,
-            albumArt49: item[49].track.album.images[0].url,
+        recentlyPlayed: {
+            albumArt0: data.items[0].track.album.images[0].url,
+            albumArt1: data.items[1].track.album.images[0].url,
+            albumArt2: data.items[2].track.album.images[0].url,
+            albumArt3: data.items[3].track.album.images[0].url,
+            albumArt4: data.items[4].track.album.images[0].url,
+            albumArt5: data.items[5].track.album.images[0].url,
+            albumArt6: data.items[6].track.album.images[0].url,
+            albumArt7: data.items[7].track.album.images[0].url,
+            albumArt8: data.items[8].track.album.images[0].url,
+            albumArt9: data.items[9].track.album.images[0].url,
+            albumArt10: data.items[10].track.album.images[0].url,
+            albumArt11: data.items[11].track.album.images[0].url,
+            albumArt12: data.items[12].track.album.images[0].url,
+            albumArt13: data.items[13].track.album.images[0].url,
+            albumArt14: data.items[14].track.album.images[0].url,
+            albumArt15: data.items[15].track.album.images[0].url,
+            albumArt16: data.items[16].track.album.images[0].url,
+            albumArt17: data.items[17].track.album.images[0].url,
+            albumArt18: data.items[18].track.album.images[0].url,
+            albumArt19: data.items[19].track.album.images[0].url,
+            albumArt20: data.items[20].track.album.images[0].url,
+            albumArt21: data.items[21].track.album.images[0].url,
+            albumArt22: data.items[22].track.album.images[0].url,
+            albumArt23: data.items[23].track.album.images[0].url,
+            albumArt24: data.items[24].track.album.images[0].url,
+            albumArt25: data.items[25].track.album.images[0].url,
+            albumArt26: data.items[26].track.album.images[0].url,
+            albumArt27: data.items[27].track.album.images[0].url,
+            albumArt28: data.items[28].track.album.images[0].url,
+            albumArt29: data.items[29].track.album.images[0].url,
+            albumArt30: data.items[30].track.album.images[0].url,
+            albumArt31: data.items[31].track.album.images[0].url,
+            albumArt32: data.items[32].track.album.images[0].url,
+            albumArt33: data.items[33].track.album.images[0].url,
+            albumArt34: data.items[34].track.album.images[0].url,
+            albumArt35: data.items[35].track.album.images[0].url,
+            albumArt36: data.items[36].track.album.images[0].url,
+            albumArt37: data.items[37].track.album.images[0].url,
+            albumArt38: data.items[38].track.album.images[0].url,
+            albumArt39: data.items[39].track.album.images[0].url,
+            albumArt40: data.items[40].track.album.images[0].url,
+            albumArt41: data.items[41].track.album.images[0].url,
+            albumArt42: data.items[42].track.album.images[0].url,
+            albumArt43: data.items[43].track.album.images[0].url,
+            albumArt44: data.items[44].track.album.images[0].url,
+            albumArt45: data.items[45].track.album.images[0].url,
+            albumArt46: data.items[46].track.album.images[0].url,
+            albumArt47: data.items[47].track.album.images[0].url,
+            albumArt48: data.items[48].track.album.images[0].url,
+            albumArt49: data.items[49].track.album.images[0].url,
           }
         })
-      }))
+      )
       .catch(error => alert('cannot retrieve spotify information right'));
   }
 
@@ -198,7 +197,9 @@ class App extends Component {
           <img src={this.state.recentlyPlayed.albumArt49} style={{ height: 150 }} alt="album art"/>
         </div>
         <div className="buttons">
+        {this.state.loggedIn &&
           <button onClick={() => this.componentDidMount()} className="button">Check Spotify Mosaic</button>
+        }
           <button onClick={() => window.location = 'https://spotify-mosaic-backend.herokuapp.com/login'} className="button">Login to Spotify</button>
         </div>
       </div>
